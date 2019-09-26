33ffed03e22867ab35535be8cbd0bf82e70775a6
KangDroid <hyunwoo286@gmail.com>
2019-09-26 17:54:32 +0900

    Add Template Test

---
+++ TemplateArrayWrapper.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <malloc/malloc.h>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+template <typename ARR>
+class ArrayWrapper {
+    private:
+        ARR type;
+        ARR * arr_mov;
+        int size;
+        int checkVal(int iterator, ARR randVal) {
+            for (int i = 0; i < iterator; i++) {
+                if (arr_mov[i] == randVal) {
+                    return -1;
+                }
+            }
+            return 0;
+        }
+    public:
+        ArrayWrapper(ARR * arr, int length) {
+            srand(time(NULL));
+            arr_mov = arr;
+            this->size = length;
+        }
+        ArrayWrapper(ARR * arr, int length, ARR clear) {
+            srand(time(NULL));
+            arr_mov = arr;
+            this->size = length;
+            for (int i = 0; i < length; i++) {
+                arr_mov[i] = clear;
+            }
+        }
+        void show() {
+            cout << "[ ";
+            for (int i = 0; i < this->size; i++) {
+                cout << arr_mov[i] << (i == (this->size - 1) ? "" : ", ");
+            }
+            cout << " ]" << endl;
+        }
+        void show(int st_range, int ed_range) { // st_range index to ed_range index(not ed_range - 1)
+            cout << "[ ";
+            for (int i = st_range; i < ed_range+1; i++) {
+                cout << arr_mov[i] << ((i == ed_range) ? "" : ", ");
+            }
+            cout << " ]" << endl;            
+        }
+        void resetValRand(int bound) {
+            for (int i = 0; i < this->size; i++) {
+                arr_mov[i] = rand() % bound;
+            }
+        }
+        void resetValRand(int bound, bool noOverlap) {
+            int randval;
+            for (int i = 0; i < this->size; i++) {
+                randval = rand() % bound;
+                if (checkVal(i, randval) == 0) {
+                    arr_mov[i] = randval;
+                } else {
+                    i--;
+                }
+            }
+        }
+        void sort() {
+            for (int a = 0; a < this->size; a++) {
+                for (int i = 0; i < (this->size) -1; i++) {
+                    if (arr_mov[i] > arr_mov[i+1]) {
+                        swap(i, i+1);
+                    }
+                }
+            }
+        }
+        void sort(bool upsc) {
+            for (int a = 0; a < this->size; a++) {
+                for (int i = 0; i < (this->size) -1; i++) {
+                    if (arr_mov[i] < arr_mov[i+1]) {
+                        swap(i, i+1);
+                    }
+                }
+            }
+        }
+        void swap(int first_index, int second_index) {
+            ARR tmp = arr_mov[first_index];
+            arr_mov[first_index] = arr_mov[second_index];
+            arr_mov[second_index] = tmp;
+        }
+        ARR *duplicateArray() {
+            ARR *tmp = new int[this->size];
+            for (int i = 0; i < this->size; i++) {
+                tmp[i] = arr_mov[i];
+            }
+            return tmp;
+        }
+};
+
+int main(void) {
+    int *array = new int[10], *array_dup;
+    char *array_two = new char[10];
+    ArrayWrapper<int> test(array, 10, 0);
+    ArrayWrapper<char> arr_char(array_two, 10, 'b');
+    //test.resetValRand(100, true);
+    arr_char.show();
+
+    //array_dup = test.duplicateArray();
+    //IntArrayWrapper tat(array_dup, 10);
+    //tat.show();
+
+
+    delete[] array_dup;
+    delete[] array;
+    return 0;
+}
